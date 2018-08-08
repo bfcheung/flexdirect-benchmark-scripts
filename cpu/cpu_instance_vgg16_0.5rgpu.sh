279138dec2e9f1ad7254cf76fd586dc2123e2b7f
James Brogan <james@bitfusion.io>
2018-08-08 11:44:35 -0700

    Increased the number of parallel instances of the 0.5 partials scripts so as to fully utilize two GPUs.

--- cpu/cpu_instance_vgg16_0.5rgpu.sh
+++ cpu/cpu_instance_vgg16_0.5rgpu.sh
@@ -1,4 +1,6 @@
 flexdirect run -n 1 -p 0.5 "python benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --local_parameter_device=gpu --variable_update=replicated --model=vgg16 --batch_size=32 --num_gpus=1 --use_nccl=false" &
 flexdirect run -n 1 -p 0.5 "python benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --local_parameter_device=gpu --variable_update=replicated --model=vgg16 --batch_size=32 --num_gpus=1 --use_nccl=false" &
+flexdirect run -n 1 -p 0.5 "python benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --local_parameter_device=gpu --variable_update=replicated --model=vgg16 --batch_size=32 --num_gpus=1 --use_nccl=false" &
+flexdirect run -n 1 -p 0.5 "python benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --local_parameter_device=gpu --variable_update=replicated --model=vgg16 --batch_size=32 --num_gpus=1 --use_nccl=false" &
 wait # block until preceeding background processes complete
 
