431d96437d97f20ee3d0d246f67e34daa96bac1b
James Brogan <james@bitfusion.io>
2018-08-08 08:58:53 -0700

    Added additional gpu scripts to run partials. Old and new partials now launch multiple concurrent applications

--- gpu/gpu_instance_rnn_ptb_0.25gpu_small.sh
+++ gpu/gpu_instance_rnn_ptb_0.25gpu_small.sh
@@ -1,2 +1,4 @@
-flexdirect run -n 1 -p 0.25 "python models/tutorials/rnn/ptb/ptb_word_lm.py --data data/simple-examples/data/ --model=small --num_gpus 1"
+flexdirect run -n 1 -p 0.25 "python models/tutorials/rnn/ptb/ptb_word_lm.py --data data/simple-examples/data/ --model=small --num_gpus 1" &
+flexdirect run -n 1 -p 0.25 "python models/tutorials/rnn/ptb/ptb_word_lm.py --data data/simple-examples/data/ --model=small --num_gpus 1" &
+wait # block until preceeding background processes complete
 
